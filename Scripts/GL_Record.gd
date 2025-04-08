ddd3f3afb907efba509855b1e25ff3676409f172
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2025-04-08 14:41:50 -0600

    more recording stuff

--- Scripts/GL_Record.gd
+++ Scripts/GL_Record.gd
@@ -1,6 +1,8 @@
 extends GL_Node
-
+var timer:float
+const sampleRate = 0.1
 var recording:Dictionary
+var oldTime:float = 0.000030042452 #sorta random number
 
 func _ready():
 	super._ready()
@@ -18,3 +20,29 @@ func _process(delta):
 	apply_pick_values()
 	for key in rows:
 		_send_input(key)
+	
+	if recording:
+		if timer <= 0:
+			timer = sampleRate
+			_traverse()
+			_record()
+		timer -= delta
+	oldTime = float(rows["Current Time"]["output"])
+
+func _traverse():
+	if float(rows["Current Time"]["output"]) == oldTime:
+		return
+	for key in recording:
+		if recording[key]["start"] == null || recording[key]["end"] == null:
+			continue
+		if recording[key]["current"] == null:
+			recording[key]["current"] = recording[key]["start"]
+		var current = recording[key]["list"][recording[key]["current"]]
+func _record():
+	pass
+
+func _create_row(name:String,input,output,picker:bool,pickDefault,pickFloatMaximum:float):
+	super._create_row(name,input,output,picker,pickDefault,pickFloatMaximum)
+	for key in rows:
+		if !recording.has(key):
+			recording[key] = {"start":null,"end":null,"current":null,"list":{}}
