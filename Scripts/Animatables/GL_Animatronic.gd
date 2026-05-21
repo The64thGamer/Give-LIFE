4089e2a65d3a12b097ba40bf40dbcb18703d875c
The64thGamer <The64thGamerMail@proton.me>
2026-05-20 21:13:24 -0600

    fixed interpolation issues on loop animations

--- Scripts/Animatables/GL_Animatronic.gd
+++ Scripts/Animatables/GL_Animatronic.gd
@@ -239,7 +239,13 @@ func _process(delta):
 		var raw_value = float(params.get("value", 0))
 		var time_value: float
 		if params["type"] == "loop":
-			time_value = fmod(lerp(old_value, raw_value, min(float(Time.get_ticks_msec() - lastTickTime) / 1000.0 / lastdelta, 1.0)), 1.0) * anim_length	
+			var delta_value = raw_value - old_value
+			if delta_value < -0.5:
+				delta_value += 1.0
+			elif delta_value > 0.5:
+				delta_value -= 0.5
+			var t = min(float(Time.get_ticks_msec() - lastTickTime) / 1000.0 / lastdelta, 1.0)
+			time_value = (old_value + delta_value * t) * anim_length
 		else:
 			time_value = clamp(lerp(old_value, raw_value, min(float(Time.get_ticks_msec() - lastTickTime) / 1000.0 / lastdelta, 1.0)), 0.0, 1.0) * anim_length
 		anim_tree.set(anim_path, time_value)
