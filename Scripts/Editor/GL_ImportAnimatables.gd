c52658152eccd2e57637b1e2d6683f34a596eaf0
The64thGamer <The64thGamerMail@proton.me>
2026-06-20 17:56:43 -0600

    wrapping up

--- Scripts/Editor/GL_ImportAnimatables.gd
+++ Scripts/Editor/GL_ImportAnimatables.gd
@@ -119,8 +119,12 @@ func _collect_lights(node: GL_Lights, group: String, color: String, channels: Di
 	_add_channel(channels, group, "intensity", GL_ChannelData.TYPE_BOOL, color)
 	if node.canChangeColor:
 		_add_channel(channels, group, "color", GL_ChannelData.TYPE_COLOR, color)
-	if node.canChangeSize and node.spotLight != null:
+	if node.canChangeSize:
 		_add_channel(channels, group, "size", GL_ChannelData.TYPE_FLOAT, color)
+	if node.canRotateX:
+		_add_channel(channels, group, "moveX", GL_ChannelData.TYPE_FLOAT, color)
+	if node.canRotateY:
+		_add_channel(channels, group, "moveY", GL_ChannelData.TYPE_FLOAT, color)
 
 func _collect_chase_light(node: GL_ChaseLight, group: String, color: String, channels: Dictionary) -> void:
 	_add_channel(channels, group, "intensity", GL_ChannelData.TYPE_BOOL,  color)
